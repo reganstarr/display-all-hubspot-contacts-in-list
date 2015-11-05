a07a83a420ae44663f49e2a7b9b67a5c4adea3ea
Regan Starr <reganstarr@users.noreply.github.com>
2015-11-05 08:26:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,22 @@ $propertiesArray = json_decode(getenv('HUBSPOT_PROPERTIES_JSON'), true);
 
 
 
+//get the name of the list
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.hubapi.com/contacts/v1/lists/$listId?hapikey=$hubspotApiKey");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($response, true);
+
+$listName = $response['name'];
+
+
+
+
+
+//get all the contacts and the requested properties
 $contactsArray = array();
 
 $vidOffset = null;
@@ -23,10 +39,9 @@ do {
 		$params['vidOffset'] = $vidOffset;
 	}
 	
-	//add the contact properties you want to include to the rest of your url query. HubSpot uses the format &property=contact&property=firstname&property=lastname for passing multiple properties
 	$query = http_build_query($params) . "&property=" . implode("&property=", $properties);
 	
-	//start the api call
+
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "https://api.hubapi.com/contacts/v1/lists/$listId/contacts/all?" . $query);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -34,12 +49,12 @@ do {
 	$response = json_decode(curl_exec($ch), true);
 	curl_close($ch);
 	
-	//loop through the response and pull out the properties you are wanting
+
 	foreach($response['contacts'] as $contact){
-		//create a temporary array for storing the information
+
 		$tempArray = array();
 		
-		//to avoid getting a "Notice" error, check that each property is actually set before trying to call its value
+
 		foreach($properties as $property){
 			if(isset($contact['properties'][$property]['value'])){
 				$tempArray[$property] = $contact['properties'][$property]['value'];
@@ -49,14 +64,13 @@ do {
 			}
 		}
 		
-		//add the temporary array to our main contacts array
+
 		array_push($contactsArray, $tempArray);
 	}
 	
-	//take note of the vid offset for your next api call in order to call the next page of contacts
+
 	$vidOffset = $response['vid-offset'];
 
-	//check if there are still more contacts to get. If there are, go through the loop again
 } while ($response['has-more'] == true);
 
 ?>
@@ -67,7 +81,7 @@ do {
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title></title>
+<title><?php echo $listName; ?></title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
@@ -76,11 +90,15 @@ do {
 <body>
 	
 <div class="container">
+  
+  <div class="page-header">
+  	<h1><?php echo $listName; ?></h1>
+  </div><!-- page-header -->
+  
 	<table class="table">
 		<thead>
 			<tr>
 				<?php
-				//loop through the properties for your column headings
 				foreach($properties as $property){
 					echo "<th>$property</th>";
 				}
@@ -89,7 +107,6 @@ do {
 		</thead>
 		<tbody>
 			<?php
-			//loop through each contact and create a row in the table
 			foreach($contactsArray as $contact){
 				echo "<tr>";
 				foreach($contact as $property){
